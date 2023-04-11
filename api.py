25f7690e1c7cd5e3687c007ab1afa1b5c5f953c1
CODE-BUDDY <111114081+BUDDY1609@users.noreply.github.com>
2023-04-12 00:47:14 +0530

    Update api.py

--- api.py
+++ api.py
@@ -3,15 +3,6 @@ import os
 import random
 from os import environ
 
-id_pattern = re.compile(r'^.\d+$')
-def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
-    else:
-        return default
-
 api_key = os.environ.get('API_KEY_1', 'jbafu/gnbjsbjdbg/febfsb').split('/')
 API = (random.choice(api_key))
 print(API)
